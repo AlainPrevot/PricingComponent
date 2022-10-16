0bd995701a5c196b4e6a1b6ab612226e424a5ece
alenchu <alain.k.prevot@gmail.com>
2022-10-16 19:21:37 -0300

    modificando Component Card

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Card } from './Card'
+import '../Sass/App.sass'
 
 export const App = () => {
   return (
-    <div style={{display: 'flex', alignItems:'center', justifyContent:'center'}}>
+    <div className='Prueba' style={{display: 'flex', alignItems:'center', justifyContent:'center'}}>
       <Card title={'Professional'} value={24.99} storage={1} memory={10} users={5}/>
-      {/* <Card title={'Professional'} value={24.99} storage={1} memory={10} users={5}/> */}
-      {/* <Card title={'Professional'} value={24.99} storage={1} memory={10} users={5}/> */}
+      <Card title={'Professional'} value={24.99} storage={1} memory={10} users={5}/>
+      <Card title={'Professional'} value={24.99} storage={1} memory={10} users={5}/>
     </div>
   )
 }
